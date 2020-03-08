1e8e5a515e2709ea07dc36a2255c763f505d8bd7
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-08 15:35:20 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,8 +1,8 @@
-function request(httpMethod, route, data) {
+function request(httpMethod, route, dataObj) {
     var responses=null;
     $.ajax({
         url: config['backend_URL'] + route,
-        data: data,
+        data: JSON.stringify(dataObj),
         type: httpMethod,
         contentType: 'application/json; charset=UTF-8',
         async: false,

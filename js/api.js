d65a74812c4a921173283e132ed45917d06623cd
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-10 15:48:33 +0800

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,19 +1,19 @@
-function request(httpMethod, route, dataObj,auth=true,headers=null) {
-    if(auth)
-        headers.Authorization='Bearer '+$.cookie('access_token');
-    var responses=null;
+function request(httpMethod, route, dataObj, auth = true, headers = null) {
+    if (auth)
+        headers.Authorization = 'Bearer ' + $.cookie('access_token');
+    var responses = null;
     $.ajax({
         url: config['backend_URL'] + route,
         data: JSON.stringify(dataObj),
         type: httpMethod,
         contentType: 'application/json; charset=UTF-8',
-        headers : headers,
+        headers: headers,
         async: false,
         complete: function (xhr) {
             responses = xhr;
         }
     });
-    if(Math.floor(responses.status/100)==5){
+    if (Math.floor(responses.status / 100) == 5) {
         $.alert({
             title: '錯誤',
             content: '系統發生錯誤!!請聯繫管理員',
@@ -21,5 +21,13 @@ function request(httpMethod, route, dataObj,auth=true,headers=null) {
             typeAnimated: true
         });
     }
-    return {'code':responses.status,'data':responses.responseJSON};
+    if (responses.status == 401 && !(httpMethod == 'POST' && route == '/oauth/token')) {
+        $.alert({
+            title: '錯誤',
+            content: '使用者驗證錯誤!!請重新登入',
+            type: 'red',
+            typeAnimated: true
+        });
+    }
+    return {'code': responses.status, 'data': responses.responseJSON};
 }

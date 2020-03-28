afc278390b1aecba20c89845e796688b2fa6e9bd
Sz-Wei Chen <swchen1217@gmail.com>
2020-03-28 17:53:33 +0800

    Update bootstrap.js

--- js/bootstrap.js
+++ js/bootstrap.js
@@ -174,7 +174,7 @@
     triggerTransitionEnd: function triggerTransitionEnd(element) {
       $(element).trigger(TRANSITION_END);
     },
-    // TODO: Remove in v5
+    // TODO0: Remove in v5
     supportsTransitionEnd: function supportsTransitionEnd() {
       return Boolean(TRANSITION_END);
     },
@@ -2415,7 +2415,7 @@
       }
     }; // ----------------------------------------------------------------------
     // the following methods are used to handle overflowing modals
-    // todo (fat): these should probably be refactored out of modal.js
+    // todo0 (fat): these should probably be refactored out of modal.js
     // ----------------------------------------------------------------------
 
 
@@ -3559,7 +3559,7 @@
           var targetBCR = target.getBoundingClientRect();
 
           if (targetBCR.width || targetBCR.height) {
-            // TODO (fat): remove sketch reliance on jQuery position/offset
+            // TODO0 (fat): remove sketch reliance on jQuery position/offset
             return [$(target)[offsetMethod]().top + offsetBase, targetSelector];
           }
         }
